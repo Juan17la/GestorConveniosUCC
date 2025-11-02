17e06a9d477f3718d39d23ea81fb3ba9f7849871
Jud1714 <juandiegolopezarias07@gmail.cm>
2025-11-02 17:33:58 -0500

    Structure file for testing

---
+++ src/test/java/com/gucc/GestorConvenioUcc/GestorConvenioUccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucc.GestorConvenioUcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorConvenioUccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
