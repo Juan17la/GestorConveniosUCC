f9544634f1d0c6139c1eb5c6c2b629d9a9b5f85b
Jud1714 <juandiegolopezarias07@gmail.cm>
2025-11-02 17:30:39 -0500

    Creating campus and user repository to start login

---
+++ src/main/java/com/gucc/GestorConvenioUcc/repository/CampusRepository.java
@@ -0,0 +1,7 @@
+package com.gucc.GestorConvenioUcc.repository;
+
+import com.gucc.GestorConvenioUcc.entity.Campus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampusRepository extends JpaRepository<Campus, Long> {
+}
