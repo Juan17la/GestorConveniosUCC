f07230466293ad0da05be0d6bd46ab99606655ae
juan17la <juandiegolopezarias07@gmail.com>
2025-11-06 17:51:54 -0500

    creating repository to RevisionAdministrativa

---
+++ src/main/java/com/gucc/GestorConvenioUcc/repository/RevisionAdministrativaRepository.java
@@ -0,0 +1,7 @@
+package com.gucc.GestorConvenioUcc.repository;
+
+import com.gucc.GestorConvenioUcc.entity.RevisionAdministrativa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RevisionAdministrativaRepository extends JpaRepository<RevisionAdministrativa, Long> {
+}
