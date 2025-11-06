c7be612f1e5a0878a1b626c2961579155b91c8b4
juan17la <juandiegolopezarias07@gmail.com>
2025-11-06 17:53:08 -0500

    Creating controller to revisionAdministrativa, two paths for creating and deleting a row in db

---
+++ src/main/java/com/gucc/GestorConvenioUcc/controllers/RevisionAdministrativaController.java
@@ -0,0 +1,28 @@
+package com.gucc.GestorConvenioUcc.controllers;
+
+import com.gucc.GestorConvenioUcc.dto.CampusDTO;
+import com.gucc.GestorConvenioUcc.dto.RevisionAdministrativaDTO;
+import com.gucc.GestorConvenioUcc.service.RevisionAdministrativaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/revision-administrativa/")
+@RequiredArgsConstructor
+public class RevisionAdministrativaController {
+
+    private final RevisionAdministrativaService revisionAdministrativaService;
+
+    @PostMapping(value = "create")
+    public ResponseEntity<RevisionAdministrativaDTO> create(@RequestBody RevisionAdministrativaDTO revisionAdministrativaDTO){
+        return ResponseEntity.ok(revisionAdministrativaService.create(revisionAdministrativaDTO));
+    }
+
+    @PostMapping(value = "delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        revisionAdministrativaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
