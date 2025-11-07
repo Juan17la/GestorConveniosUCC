6e5f1c653ec9f38658fb861e0cfde7a684e2c722
juan17la <juandiegolopezarias07@gmail.com>
2025-11-06 21:07:45 -0500

    Creating Revision Juridica Repository

---
+++ src/main/java/com/gucc/GestorConvenioUcc/repository/RevisionJuridicaRepository.java
@@ -0,0 +1,7 @@
+package com.gucc.GestorConvenioUcc.repository;
+
+import com.gucc.GestorConvenioUcc.entity.RevisionJuridica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RevisionJuridicaRepository extends JpaRepository<RevisionJuridica, Long> {
+}
