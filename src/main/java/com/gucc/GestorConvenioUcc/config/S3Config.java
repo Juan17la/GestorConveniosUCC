64047fd36714b1b7934ef9b7b0cbbf61154ee560
juan17la <juandiegolopezarias07@gmail.com>
2025-11-09 15:04:14 -0500

    Creating S3 configuration sync and S3 service to upload files

---
+++ src/main/java/com/gucc/GestorConvenioUcc/config/S3Config.java
@@ -0,0 +1,34 @@
+package com.gucc.GestorConvenioUcc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.access.key}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret.key}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    // CLIENTE S3 Síncrono
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+                .build();
+    }
+}
