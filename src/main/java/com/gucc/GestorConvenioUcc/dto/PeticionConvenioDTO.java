5e19c4d216cf8228931fcbcfb8e2e16c838716cd
juan17la <juandiegolopezarias07@gmail.com>
2025-11-12 08:37:28 -0500

    Refactor Peticion DTO to set fehcaCrecion as a Date type, and deleting useless

--- src/main/java/com/gucc/GestorConvenioUcc/dto/PeticionConvenioDTO.java
+++ src/main/java/com/gucc/GestorConvenioUcc/dto/PeticionConvenioDTO.java
@@ -4,18 +4,17 @@ import com.gucc.GestorConvenioUcc.enums.EstadoPeticion;
 import com.gucc.GestorConvenioUcc.enums.TipoConvenio;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class PeticionConvenioDTO {
     private Long id;
-    private LocalDateTime fechaCreacion;
+    private Date fechaCreacion;
     private TipoConvenio tipo;
     private EstadoPeticion estado;
     private Long creadorId;
-    private String creadorNombre;
     private Long campusId;
-    private String campusNombre;
     private RevisionAdministrativaDTO revisionAdministrativa;
     private RevisionJuridicaDTO revisionJuridica;
     private FirmaConvenioDTO firmaConvenio;
