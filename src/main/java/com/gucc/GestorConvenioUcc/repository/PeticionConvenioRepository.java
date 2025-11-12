e312c33561d0682d289d873abd68e5f3c19251b4
juan17la <juandiegolopezarias07@gmail.com>
2025-11-12 08:42:50 -0500

    Create Controller for peticion convenio

---
+++ src/main/java/com/gucc/GestorConvenioUcc/repository/PeticionConvenioRepository.java
@@ -0,0 +1,7 @@
+package com.gucc.GestorConvenioUcc.repository;
+
+import com.gucc.GestorConvenioUcc.entity.PeticionConvenio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeticionConvenioRepository extends JpaRepository<PeticionConvenio, Long> {
+}
