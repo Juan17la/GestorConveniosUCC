48504d4e4b33bc4edcdd9c2a7151be955b4841f1
juan17la <juandiegolopezarias07@gmail.com>
2025-11-17 21:08:54 -0500

    Refactor to Estado Convenio to only leave the state that are useful for already existing convenios

--- src/main/java/com/gucc/GestorConvenioUcc/enums/EstadoConvenio.java
+++ src/main/java/com/gucc/GestorConvenioUcc/enums/EstadoConvenio.java
@@ -1,10 +1,6 @@
 package com.gucc.GestorConvenioUcc.enums;
 
 public enum EstadoConvenio {
-    BORRADOR,
-    EN_REVISION_JURIDICA,
-    EN_VOBO_RECTORIA_CAMPUS,
-    EN_FIRMA_NACIONAL,
     ACTIVO,
     POR_VENCER,
     VENCIDO
