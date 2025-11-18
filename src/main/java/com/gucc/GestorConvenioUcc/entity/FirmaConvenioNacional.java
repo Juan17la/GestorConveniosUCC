64f1c5d30232b22e31ed3163cb94134d3f8e6ad1
juan17la <juandiegolopezarias07@gmail.com>
2025-11-17 21:06:48 -0500

    Refactor to FirmaConvenioNacional to create fechaCreacion automatically

--- src/main/java/com/gucc/GestorConvenioUcc/entity/FirmaConvenioNacional.java
+++ src/main/java/com/gucc/GestorConvenioUcc/entity/FirmaConvenioNacional.java
@@ -38,4 +38,9 @@ public class FirmaConvenioNacional {
 
     @Column(nullable = true, columnDefinition = "TEXT")
     private String mensajeRechazo;
+
+    @PrePersist
+    protected void onCreate() {
+        fechaCreacion = LocalDateTime.now();
+    }
 }
